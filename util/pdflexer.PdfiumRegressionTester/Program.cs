b05aeef8cf8c1e18f54837f28eb9ea150d714c14
Michael Plaisted <20848495+plaisted@users.noreply.github.com>
2022-06-28 20:39:44 -0500

    add commandline and logs to reg testing

--- util/pdflexer.PdfiumRegressionTester/Program.cs
+++ util/pdflexer.PdfiumRegressionTester/Program.cs
@@ -5,51 +5,163 @@ using PdfLexer.DOM;
 using PdfLexer.Filters;
 using PdfLexer.Operators;
 using PdfLexer.Serializers;
+using System.CommandLine;
 
-Directory.CreateDirectory(args[1]);
 
-var counts = new List<(string, int, int)>();
+
+StreamWriter writer = null;
+
+void Log(string message)
 {
-    var modified = Path.Combine(args[1], "all.pdf");
-    using var fo = File.Create(modified);
-    using var sw = new StreamingWriter(fo);
-    
-    foreach (var file in Directory.GetFiles(args[0], "*.pdf"))
-    {
-        using var fr = File.OpenRead(file);
-        using var doc = PdfDocument.Open(fr);
-        if (doc.Trailer.Get(PdfName.Encrypt) != null)
-        {
-            continue;
-        }
-        var start = sw.PageCount;
-        foreach (var pg in doc.Pages)
-        {
-            sw.AddPage(pg);
-        }
-        counts.Add((file, start, sw.PageCount));
-    }
-    sw.Complete(new PdfDictionary());
+    writer?.WriteLine(message);
+    Console.WriteLine(message);
 }
 
+var output = new Option<string>(
+            name: "--output",
+            description: "Path where results should be stored.");
+
+var pdfRoot = new Option<string>(
+            name: "--pdfs",
+            description: "Path where pdfs to run are located.");
+
+var type = new Option<string>(
+            name: "--type",
+            description: "Type of test to run, merge or rebuild.");
+
+var rootCommand = new RootCommand("PDF regression testing");
+rootCommand.AddOption(pdfRoot);
+rootCommand.AddOption(output);
+rootCommand.AddOption(type);
+
+int returnCode = 0;
+
+rootCommand.SetHandler((type, root, prefix) =>
 {
-    
-    using var allfs = File.OpenRead(Path.Combine(args[1], "all.pdf"));
-    using var all = PdfDocument.Open(allfs);
-    foreach (var (pdf, start, end) in counts)
+    returnCode = RunBase(type, root, prefix);
+}, type, pdfRoot, output);
+
+await rootCommand.InvokeAsync(args);
+
+return returnCode;
+
+int RunBase(string type, string pdfRoot, string output)
+{
+    Directory.CreateDirectory(output);
+    using var lo = File.Create(Path.Combine(output, type.ToLower() + ".log"));
+    writer = new StreamWriter(lo);
+    using var _ = writer;
+    switch (type.ToUpper())
     {
-        var modified = Path.Combine(args[1], Path.GetFileName(pdf));
+        case "MERGE":
+            return RunMergeTests(pdfRoot, output) ? 0 : 1;
+        case "REBUILD":
+            return RunRebuildTests(pdfRoot, output) ? 0 : 1;
+        default:
+            Console.WriteLine("Unknown test type: " + type);
+            return 1;
+    }
+}
+
+bool RunMergeTests(string pdfRoot, string output) 
+{
+    bool success = true;
+    var merged = Path.Combine(output, "all.pdf");
+
+    var counts = new List<(string, int, int)>();
+    {
+        
+        using var fo = File.Create(merged);
+        using var sw = new StreamingWriter(fo);
+
+        foreach (var file in Directory.GetFiles(pdfRoot, "*.pdf"))
         {
-            using var doc = PdfDocument.Create();
-            for (var i = start;i < end;i++)
+            using var fr = File.OpenRead(file);
+            using var doc = PdfDocument.Open(fr);
+            if (doc.Trailer.Get(PdfName.Encrypt) != null)
             {
-                doc.Pages.Add(all.Pages[i]);
+                continue;
+            }
+            var start = sw.PageCount;
+            foreach (var pg in doc.Pages)
+            {
+                sw.AddPage(pg);
+            }
+            counts.Add((file, start, sw.PageCount));
+        }
+        sw.Complete(new PdfDictionary());
+    }
+
+    {
+        using var allfs = File.OpenRead(merged);
+        using var all = PdfDocument.Open(allfs);
+        foreach (var (pdf, start, end) in counts)
+        {
+            var modified = Path.Combine(pdfRoot, Path.GetFileName(pdf));
+            {
+                using var doc = PdfDocument.Create();
+                for (var i = start; i < end; i++)
+                {
+                    doc.Pages.Add(all.Pages[i]);
+                }
+                using var fo = File.Create(modified);
+                doc.SaveTo(fo);
+            }
+
+            var comparer = new Compare(Path.Combine(output, Path.GetFileNameWithoutExtension(pdf)), 2);
+            var pgs = comparer.CompareAllPages(pdf, modified);
+            var nm = Path.GetFileName(pdf);
+            bool changes = false;
+            for (var i = 0; i < pgs.Count; i++)
+            {
+                var pg = pgs[i];
+                if (pg.HadChanges)
+                {
+                    changes = true;
+                    Log($"[{nm}] failed pg {i} -> {pg.Type.ToString()}");
+                }
+            }
+            if (!changes)
+            {
+                success = false;
+                File.Delete(modified);
+                Log($"[{nm}] passed");
+            }
+            else
+            {
+                Log($"[{nm}] failed");
+                File.Copy(pdf, Path.Combine(pdfRoot, Path.GetFileNameWithoutExtension(pdf) + "_baseline.pdf"), true);
+            }
+        }
+    }
+    File.Delete(merged);
+    return success;
+}
+
+bool RunRebuildTests(string pdfRoot, string output)
+{
+    bool success = true;
+    Directory.CreateDirectory(output);
+
+    foreach (var pdf in Directory.GetFiles(pdfRoot, "*.pdf"))
+    {
+        var comparer = new Compare(Path.Combine(output, Path.GetFileNameWithoutExtension(pdf)), 2);
+        var modified = Path.Combine(output, Path.GetFileName(pdf));
+        {
+            using var doc = PdfDocument.Open(File.ReadAllBytes(pdf));
+            if (doc.Trailer.Get(PdfName.Encrypt) != null)
+            {
+                continue;
             }
             using var fo = File.Create(modified);
-            doc.SaveTo(fo);
+            var sw = new StreamingWriter(fo, true, true);
+            foreach (var pg in doc.Pages)
+            {
+                var np = ReWriteStream(doc, pg);
+                sw.AddPage(np);
+            }
+            sw.Complete(doc.Trailer.CloneShallow(), doc.Catalog.CloneShallow());
         }
-
-        var comparer = new Compare(Path.Combine(args[1], Path.GetFileNameWithoutExtension(pdf)), 2);
         var pgs = comparer.CompareAllPages(pdf, modified);
         var nm = Path.GetFileName(pdf);
         bool changes = false;
@@ -58,61 +170,31 @@ var counts = new List<(string, int, int)>();
             var pg = pgs[i];
             if (pg.HadChanges)
             {
-                changes = true;
-                Console.WriteLine($"[{nm}] {i} {pg.Type.ToString()}");
+                if (pg.Type == ChangeType.ErrorBaseline)
+                {
+                    Log($"[{nm}] pdfium failed to open pg on baseline{i}");
+                } else
+                {
+                    changes = true;
+                    Log($"[{nm}] failed pg {i} -> {pg.Type.ToString()}");
+                }
+                
             }
         }
         if (!changes)
         {
             File.Delete(modified);
+            Log($"[{nm}] passed");
         }
         else
         {
-            File.Copy(pdf, Path.Combine(args[1], Path.GetFileNameWithoutExtension(pdf) + "_baseline.pdf"), true);
+            Log($"[{nm}] failed");
+            File.Copy(pdf, Path.Combine(output, Path.GetFileNameWithoutExtension(pdf) + "_baseline.pdf"), true);
         }
     }
-
+    return success;
 }
 
-foreach (var pdf in Directory.GetFiles(args[0], "*.pdf"))
-{
-    var comparer = new Compare(Path.Combine(args[1], Path.GetFileNameWithoutExtension(pdf)), 2);
-    var modified = Path.Combine(args[1], Path.GetFileName(pdf));
-    {
-        using var doc = PdfDocument.Open(File.ReadAllBytes(pdf));
-        if (doc.Trailer.Get(PdfName.Encrypt) != null)
-        {
-            continue;
-        }
-        using var fo = File.Create(modified);
-        var sw = new StreamingWriter(fo, true, true);
-        foreach (var pg in doc.Pages)
-        {
-            var np = ReWriteStream(doc, pg);
-            sw.AddPage(np);
-        }
-        sw.Complete(doc.Trailer.CloneShallow(), doc.Catalog.CloneShallow());
-    }
-    var pgs = comparer.CompareAllPages(pdf, modified);
-    var nm = Path.GetFileName(pdf);
-    bool changes = false;
-    for (var i = 0; i < pgs.Count; i++)
-    {
-        var pg = pgs[i];
-        if (pg.HadChanges)
-        {
-            changes = true;
-            Console.WriteLine($"[{nm}] {i} {pg.Type.ToString()}");
-        }
-    }
-    if (!changes)
-    {
-        File.Delete(modified);
-    } else
-    {
-        File.Copy(pdf, Path.Combine(args[1], Path.GetFileNameWithoutExtension(pdf) + "_baseline.pdf"), true);
-    }
-}
 
 
 static PdfPage ReWriteStream(PdfDocument doc, PdfPage page)
