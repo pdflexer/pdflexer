75c41e01ee41cbb37f93063a5875931000d68807
Michael Plaisted <20848495+plaisted@users.noreply.github.com>
2022-08-14 10:39:28 -0500

    fix internal ref

--- src/PdfLexer/ExtInit.cs
+++ src/PdfLexer/ExtInit.cs
@@ -2,6 +2,7 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("PdfLexer.Tests")]
+[assembly: InternalsVisibleTo("PdfLexer.Images")] // shouldn't need this but will need to clean up images / colorspace refs
 [assembly: InternalsVisibleTo("PdfLexer.Benchmarks")]
 
 namespace System.Runtime.CompilerServices
