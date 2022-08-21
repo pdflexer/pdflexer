1a2b7bc7142ac5256446eef15189ca5e191819a7
Michael Plaisted <20848495+plaisted@users.noreply.github.com>
2022-08-21 18:47:40 -0500

    some truetype coverage

--- test/PdfLexer.Tests/TextExtractionTests.cs
+++ test/PdfLexer.Tests/TextExtractionTests.cs
@@ -21,6 +21,9 @@ namespace PdfLexer.Tests
         public void It_Reads_Type1_Embedded_No_Encoding_FF3() => RunSingle("issue11279.pdf");
 
         [Fact]
+        public void It_Reads_Type1_Embedded_Diff_Encoding_FF3() => RunSingle("issue11016_reduced.pdf");
+
+        [Fact]
         public void It_Reads_Type1_B14() => RunSingle("asciihexdecode.pdf");
 
         [Fact]
@@ -49,6 +52,59 @@ namespace PdfLexer.Tests
 
         [Fact]
         public void It_Reads_Type1_Embedded_Diff_Base_WinAnsi_FF3() => RunSingle("083014_hanrei.pdf");
+
+
+        // [Fact]
+        //public void It_Reads_TrueType_Bad_Base14() => RunSingle("issue6127.pdf");
+        [Fact]
+        public void It_Reads_TrueType_MacRoman_Embedded() => RunSingle("openoffice.pdf");
+
+        [Fact]
+        public void It_Reads_TrueType_MacRoman_NonEmbed() => RunSingle("issue6127.pdf");
+
+        [Fact]
+        public void It_Reads_TrueType_MacRoman_Embedded_ToUnicode() => RunSingle("issue8707.pdf");
+
+        [Fact]
+        public void It_Reads_TrueType_WinAnsi() => RunSingle("bug1671312_ArialNarrow.pdf");
+
+        [Fact]
+        public void It_Reads_TrueType_WinAnsi_Embedded() => RunSingle("annotation-border-styles.pdf");
+
+
+        // TODO research, doens't look like extractable
+        // [Fact]
+        // public void It_Reads_TrueType_WinAnsi_Embedded_widths_mw() => RunSingle("bug1027533.pdf");
+
+        [Fact]
+        public void It_Reads_TrueType_WinAnsi_NonEmbed() => RunSingle("issue5470.pdf");
+
+        [Fact]
+        public void It_Reads_TrueType_WinAnsi_NonEmbed_ToUnicode() => RunSingle("issue4934.pdf");
+
+        [Fact]
+        public void It_Reads_TrueType_Identity_Embedded_ToUnicode() => RunSingle("issue5701.pdf");
+
+        [Fact]
+        public void It_Reads_TrueType_Diff_Embedded() => RunSingle("bug1669099.pdf");
+
+        [Fact]
+        public void It_Reads_TrueType_Diff_Embedded_ToUnicode() => RunSingle("bug1132849.pdf");
+
+        [Fact]
+        public void It_Reads_TrueType_None_Embedded_ToUnicode() => RunSingle("annotation-highlight-without-appearance.pdf");
+        
+        // [Fact]
+        // public void It_Reads_TrueType_None_Embedded() => RunSingle("bug894572.pdf");
+
+        [Fact]
+        public void It_Reads_TrueType_None_NonEmbed_ToUnicode() => RunSingle("bug864847.pdf");
+
+        [Fact]
+        public void It_Reads_TrueType_WinAnsi_FF3() => RunSingle("issue4668.pdf");
+
+
+
 
         private void RunSingle(string name)
         {
