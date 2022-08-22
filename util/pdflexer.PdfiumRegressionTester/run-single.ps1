2ce45493d63559ab4e223e82d70ae50c96412e58
Michael Plaisted <20848495+plaisted@users.noreply.github.com>
2022-08-22 08:14:51 -0500

    some truetype coverage (#54)

--- util/pdflexer.PdfiumRegressionTester/run-single.ps1
+++ util/pdflexer.PdfiumRegressionTester/run-single.ps1
@@ -1,4 +1,4 @@
 
 $outputPath = "$PSScriptRoot/../../test/results/txt"
-$pdf = "$PSScriptRoot/../../test/pdfs/pdfjs/issue3207r.pdf"
+$pdf = "$PSScriptRoot/../../test/pdfs/pdfjs/issue4668.pdf"
 .\bin\Release\net6.0\publish\pdflexer.PdfiumRegressionTester.exe --strict --type text --output $outputPath --pdf $pdf
