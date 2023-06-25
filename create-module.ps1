e96db95ef853379739caa5d427e79129cc2a1b3c
Michael Plaisted <20848495+plaisted@users.noreply.github.com>
2023-06-25 17:18:10 -0500

    remove old pwsh

--- create-module.ps1
+++ create-module.ps1
@@ -1,14 +1,8 @@
 Push-Location $PSScriptRoot
 try {
-    dotnet publish -c release -f net6.0 src/PdfLexer.Powershell
     dotnet publish -c release -f net7.0 src/PdfLexer.Powershell
-    dotnet publish -c release -f netstandard2.1 src/PdfLexer.Powershell
     [IO.Directory]::CreateDirectory("$pwd/.psm/PdfLexer/net7.0")
-    [IO.Directory]::CreateDirectory("$pwd/.psm/PdfLexer/net6.0")
-    [IO.Directory]::CreateDirectory("$pwd/.psm/PdfLexer/netstandard2.1")
     Copy-Item ./src/PdfLexer.Powershell/bin/release/net7.0/publish/* ./.psm/PdfLexer/net7.0/
-    Copy-Item ./src/PdfLexer.Powershell/bin/release/net6.0/publish/* ./.psm/PdfLexer/net6.0/
-    Copy-Item ./src/PdfLexer.Powershell/bin/release/netstandard2.1/publish/* ./.psm/PdfLexer/netstandard2.1/
     Copy-Item ./src/PdfLexer.Powershell/PdfLexer.psd1 ./.psm/PdfLexer/
     Copy-Item ./src/PdfLexer.Powershell/PdfLexer.psm1 ./.psm/PdfLexer/
 }
--- src/PdfLexer.Powershell/PdfLexer.psm1
+++ src/PdfLexer.Powershell/PdfLexer.psm1
@@ -5,18 +5,11 @@ $PSModuleRoot = $PSModule.ModuleBase
 $binaryModuleRoot = $PSModuleRoot
 
 $mv = $PSVersionTable.PSVersion;
-if ($mv -lt [version]'7.0') {
-    throw "PdfLexer is not supported on your powershell version. PS 7.0+ is required."
+if ($mv -lt [version]'7.3') {
+    throw "PdfLexer is not supported on your powershell version. PS 7.3+ is required."
 
 }
-elseif ($mv -lt [version]'7.2') {
-    $binaryModuleRoot = Join-Path -Path $PSModuleRoot -ChildPath 'netstandard2.1'
-}
-elseif ($mv.Major -eq 7 -and $mv.Minor -eq 2) {
-    $binaryModuleRoot = Join-Path -Path $PSModuleRoot -ChildPath 'net6.0'
-} else {
-    $binaryModuleRoot = Join-Path -Path $PSModuleRoot -ChildPath 'net7.0'
-}
+$binaryModuleRoot = Join-Path -Path $PSModuleRoot -ChildPath 'net7.0'
 
 $binaryModulePath = Join-Path -Path $binaryModuleRoot -ChildPath 'PdfLexer.Powershell.dll'
 $binaryModule = Import-Module -Name $binaryModulePath -PassThru
